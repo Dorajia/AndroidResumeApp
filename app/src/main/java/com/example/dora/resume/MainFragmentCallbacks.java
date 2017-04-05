1d25f5f301b3f8c61aa5b19b30c7351d0795d226
Dorajia <yuanyuan.jia@sjsu.edu>
2017-04-05 11:18:39 -0700

    Update app

---
+++ app/src/main/java/com/example/dora/resume/MainFragmentCallbacks.java
@@ -0,0 +1,8 @@
+package com.example.dora.resume;
+
+/**
+ * Interface implemented by MainActivity and used by AMainFragment, BasicInfoFragment and ProjectShowcaseFragment.
+ */
+public interface MainFragmentCallbacks {
+    public void displayBActivity();
+}
